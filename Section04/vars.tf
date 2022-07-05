43ab366627d8232741ea9747003f36c5b1bd895c
Connell McGinley <connell@Connells-MacBook-Air.local>
2022-07-05 22:00:04 +0100

    updating files

--- Section04/vars.tf
+++ Section04/vars.tf
@@ -1,13 +1,13 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
-variable "AWS_REGION" [
+variable "AWS_REGION" {
     default = "eu-west-1"
-]
+}
 variable "AMIS" {
-    type = "map"
+    type = map(string)
     default = {
         us-east-1 = "ami-13b3557e"
         us-west-2 = "ami-06b94666"
-        eu-west-1 = "ami-0d729a60"
+        eu-west-1 = "ami-0f29c8402f8cce65c"
     }
 }
